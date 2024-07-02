f2bdf8e10714e4e259a33282c45879b6f595c445
Jor1005 <abanto1005@gmail.com>
2024-07-01 21:18:28 -0500

    simluacion completada

--- js/mostrarProductos.js
+++ js/mostrarProductos.js
@@ -1,17 +1,21 @@
-import { conexionAPI } from "./conexionAPI";
+import { conexionAPI } from "./conexionAPI.js";
 
 const lista = document.querySelector("[data-lista]");
 
-function crearCard(){
+function crearCard(nombre,precio,imagen){
     const producto = document.createElement ("li");
     producto.className= "product";
-    producto.innerHTML= `<img src="Assets\Stormtrooper.jpg" alt="Stormtrooper">
-                         <h3>Stormtrooper</h3>
-                         <p>$ 60,00</p>`;
+    producto.innerHTML= `<img src="${imagen}" alt="Stormtrooper">
+                         <h3>"${nombre}"</h3>
+                         <p>$"${precio}" </p>`;
 
     return producto;
 }
 
 async function listarProductos (){
-    const listAPI = conexionAPI.ListarProductos()
+    const listAPI = await conexionAPI.ListarProductos()
+
+    listAPI.forEach(producto=>lista.appendChild(crearCard(producto.nombre,producto.precio,producto.imagen)))
 }
+
+listarProductos();
